f67cfa447c62206a44b036796e707176bebb5ccd
HippoSecurity <hippopass@gmail.com>
2017-04-15 19:20:50 +0800

    Update header file

--- hippo.h
+++ hippo.h
@@ -1,17 +1,34 @@
-/* Copyright (C) 2017 HippoSecurity */
 #ifndef HIPPO_H
 #define HIPPO_H
+
+#ifdef __unix__
+#define OS_Unix
+#elif defined(_WIN32) || defined(_WIN64)
+#define OS_Windows
+#endif
 
 # ifdef __cplusplus
 extern "C"
 {
 # endif
 
-int hippo_init(char* apiKey);
-size_t hippo_get_gcode(int handle, char* buf, size_t len);
-int hippo_verify_otp(int handle, char* licenseKey, char* hostId, char* otp);
-int hippo_get_license_count(int handle);
-void hippo_done(int handle);
+#ifdef OS_Windows
+#ifdef BUILDING_HIPPO_DLL
+#define HIPPO_DLL __declspec(dllexport)
+#else
+#define HIPPO_DLL __declspec(dllimport)
+#endif /* BUILDING_HIPPO_DLL */
+#endif /* OS_Windows */
+
+#ifdef OS_Unix
+#define HIPPO_DLL
+#endif /* OS_Unix */
+
+int HIPPO_DLL hippo_init(char* apiKey);
+size_t HIPPO_DLL hippo_get_gcode(int handle, char* buf, size_t len);
+int HIPPO_DLL hippo_verify_otp(int handle, char* licenseKey, char* hostId, char* otp);
+int HIPPO_DLL hippo_get_license_count(int handle);
+void HIPPO_DLL hippo_done(int handle);
 
 # ifdef __cplusplus
 }
